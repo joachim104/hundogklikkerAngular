d115e8cfb896172cd155006760137b8c51824a04
joachim104 <joachim.stougaard@hotmail.com>
2019-05-06 10:24:11 +0200

    first commit

---
+++ src/app/material.ts
@@ -0,0 +1,16 @@
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule, MatFormFieldModule, MatInputModule],
+
+    exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule, MatFormFieldModule, MatInputModule],
+})
+export class MaterialModule { }
